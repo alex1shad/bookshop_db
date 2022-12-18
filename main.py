200e432512ac278544f99daca007b08182aef161
Alex1Shad <alex1shad@yandex.ru>
2022-12-18 22:25:10 +0300

    3 commit

--- main.py
+++ main.py
@@ -4,17 +4,11 @@ from sqlalchemy.orm import sessionmaker
 import models as m
 
 
-with open('user_info.txt', 'rt') as user_file, open('fixtures/tests_data.json', 'r') as data_file:
-    db_type = user_file.readline().replace('db_type=', '').strip()
-    login = user_file.readline().replace('login=', '').strip()
-    password = user_file.readline().replace('password=', '').strip()
-    host_name = user_file.readline().replace('hostname=', '').strip()
-    db_port = user_file.readline().replace('db_port=', '').strip()
-    db_name = user_file.readline().replace('db_name=', '').strip()
+with open('user_info.json', 'r') as user_file, open('fixtures/tests_data.json', 'r') as data_file:
+    db_type, login, password, hostname, db_port, db_name =  json.load(user_file).values()
     book_data = json.load(data_file)
 
-
-DSN = f'{db_type}://{login}:{password}@{host_name}:{db_port}/{db_name}'
+DSN = f'{db_type}://{login}:{password}@{hostname}:{db_port}/{db_name}'
 engine = sqlalchemy.create_engine(DSN)
 m.create_table(engine)
 
@@ -42,10 +36,12 @@ def book_info():
                 while (True):
                     user_choice = str(input('Введите id:\n')).strip()
                     if user_choice.isdigit() and user_choice != '':
-                        qw = session.query(m.Publisher.publisher_id, m.Book.book_title, m.Shop.shop_name,
-                            m.Sale.book_price, m.Sale.date_sale).\
-                            join(m.Book).join(m.Stock).join(m.Shop).join(m.Sale).\
-                            filter(m.Publisher.publisher_id == int(user_choice))
+                        res = session.query(m.Book.book_title, m.Shop.shop_name, m.Sale.book_price, m.Sale.date_sale).\
+                                    join(m.Publisher, m.Book.publisher_id==m.Publisher.publisher_id).\
+                                    join(m.Stock, m.Stock.book_id==m.Book.book_id).\
+                                    join(m.Sale, m.Sale.stock_id==m.Stock.stock_id).\
+                                    join(m.Shop, m.Shop.shop_id==m.Stock.shop_id).\
+                                    filter(m.Publisher.publisher_id==user_choice)
                         break
                     else:
                         print('Ответ введен некорректно. id должен состоять только из цифр.')
@@ -54,10 +50,12 @@ def book_info():
                 while (True):
                     user_choice = str(input('Введите фамилию:\n')).strip()
                     if user_choice != '':
-                        qw = session.query(m.Publisher.publisher_name, m.Book.book_title, m.Shop.shop_name,
-                            m.Sale.book_price, m.Sale.date_sale).\
-                            join(m.Book).join(m.Stock).join(m.Shop).join(m.Sale).\
-                            filter(m.Publisher.publisher_name.ilike(user_choice))
+                        res = session.query(m.Book.book_title, m.Shop.shop_name, m.Sale.book_price, m.Sale.date_sale).\
+                                    join(m.Publisher, m.Book.publisher_id==m.Publisher.publisher_id).\
+                                    join(m.Stock, m.Stock.book_id==m.Book.book_id).\
+                                    join(m.Sale, m.Sale.stock_id==m.Stock.stock_id).\
+                                    join(m.Shop, m.Shop.shop_id==m.Stock.shop_id).\
+                                    filter(m.Publisher.publisher_name==user_choice)
                         break
                     else:
                         print('Введите что-нибудь.')
@@ -67,22 +65,8 @@ def book_info():
 
         print()
         print('Название книги' + ' ' * 36, '|', 'Магазин' + ' ' * 8, '|', 'Цена' + ' ' * 4, '|', 'Дата продажи')
-        print('-' * 50, '|', '-' * 15, '|', '-' * 8, '|', '-' * 13)
-        for el in qw:
-            if len(el[1]) != 50:
-                book_name = el[1] + ' ' * (50 - len(el[1]))
-            else:
-                book_name = el[1]
-            if len(el[2]) != 15:
-                shop_name = el[2] + ' ' * (15 - len(el[2]))
-            else:
-                shop_name = el[2]
-            if len(str(el[3])) != 8:
-                book_price = str(el[3]) + ' ' * (8 - len(str(el[3])))
-            else:
-                book_price = str(el[3])
-            data_without_time = str(el[4]).split()[0]
-            print(f'{book_name} | {shop_name} | {book_price} | {data_without_time}')
+        for book, shop, price, date in res:
+            print(f'{book: <50} | {shop: <15} | {price: <8} | {date: <13}')
         print('-' * 50, '|', '-' * 15, '|', '-' * 8, '|', '-' * 13)
         while (True):
             final_choice = str(input('Введите "1", чтобы продолжить; введите "2", чтобы выйти:\n'))
